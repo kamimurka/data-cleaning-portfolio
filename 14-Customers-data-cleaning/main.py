1d04aa4e900ed29d4cc5649b2ff6ba6fd35e5671
Mark Tymchenko <me.timchenko@gmail.com>
2026-09-24 19:48:45 +0300

    Pre-last

--- 14-Customers-data-cleaning/main.py
+++ 14-Customers-data-cleaning/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import phonenumbers
+import re
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -37,15 +38,43 @@ country_mapping = {
 
 df['country'] = df['country'].replace(country_mapping)
 
+# Cleaning phone column
+def clean_phone(raw, region):
+    if pd.isna(raw) or pd.isna(region):
+        return None
+    
+    raw = raw.strip()
+    raw = str(raw)
+    if raw.startswith('00'):
+        raw = '+' + raw[2:]
+    raw = re.sub(r'\s*(ext\.?|доб\.?)\s*\d+', '', raw, flags=re.IGNORECASE)
+
+    try:
+        phone = phonenumbers.parse(raw, region)
+        if phonenumbers.is_valid_number(phone):
+            return phonenumbers.format_number(phone, phonenumbers.PhoneNumberFormat.E164)
+        else:
+            print(phonenumbers.format_number(phone, phonenumbers.PhoneNumberFormat.E164))
+    except phonenumbers.NumberParseException:
+        return None
+
+df['phone'] = df.apply(lambda row: clean_phone(row['phone'], row['country']), axis=1)
+
+print(f'NA are: {df['phone'].isna().sum()}')
 
 # Cleaning signup_date column
-idx = pd.to_datetime(df['signup_date'],
+df['signup_date'] = pd.to_datetime(df['signup_date'],
     format='mixed',
-    errors='coerce').idxmax()
+    errors='coerce')
 
-print(df.loc[idx])
+df.loc[df["signup_date"] > pd.Timestamp.now(), "signup_date"] = pd.NaT
 
 
 # print(df['country'].value_counts())
 # print('-'*100)
-# print(df['signup_date'].max())
+
+# Finalization
+df2 = pd.read_csv('14-Customers-data-cleaning/customers_dirty.csv', engine='python')
+print(df2.loc[df['phone'].isna(), 'phone'].tolist())
+# Exporting
+# pd.to_csv('customers_cleaned.csv', index=False)
